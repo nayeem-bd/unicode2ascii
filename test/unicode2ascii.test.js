dc9b88c45c86492a305c721dd048ae657f0910d7
nayeem <nimuzzamanj@gmail.com>
2024-09-07 03:55:25 +0600

    ascii to unicode added

--- test/unicode2ascii.test.js
+++ test/unicode2ascii.test.js
@@ -1,12 +1,22 @@
-import { ConvertToASCII } from "../dist/unicode2ascii.esm.js";
+import { ConvertToASCII,ConvertToUnicode } from "../src/index.js";
 import assert from "assert";
 import testcase from "./testcase.js";
 
+// unicode to bijoy
 testcase.forEach((el) => {
   assert.strictEqual(
     ConvertToASCII("bijoy", el.unicode),
     el.ascii,
     "Unicode to Bijoy conversion failed"
+  );
+});
+
+// bijoy to unicode
+testcase.forEach((el) => {
+  assert.strictEqual(
+    ConvertToUnicode("bijoy", el.ascii),
+    el.unicode,
+    "Bijoy to Unicode conversion failed"
   );
 });
 
